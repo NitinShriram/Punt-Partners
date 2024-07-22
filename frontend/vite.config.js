6ab7d0562ae32205e9a4e729b49bbb5dbc4f2bf3
Nitin Shriram <nitinshriram2212@gmail.com>
2024-07-22 19:37:32 +0530

    commit 4

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,9 +1,6 @@
 import { defineConfig } from "vite";
-import vercel from "vite-plugin-vercel";
+import react from "@vitejs/plugin-react-swc";
 
 export default defineConfig({
-  server: {
-    port: 5173,
-  },
-  plugins: [vercel()],
+  plugins: [react()],
 });
